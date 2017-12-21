52a37a0a4a895cf8e3a4ed4a33ffd921333adc45
heweigeng1 <6318275@qq.com>
2017-12-21 16:12:31 +0800

    React 后台UI框架

---
+++ Xprepay.Web.Main/ant-design-pro/src/layouts/BasicLayout.js
@@ -0,0 +1,161 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Layout, Icon } from 'antd';
+import DocumentTitle from 'react-document-title';
+import { connect } from 'dva';
+import { Route, Redirect, Switch } from 'dva/router';
+import { ContainerQuery } from 'react-container-query';
+import classNames from 'classnames';
+import GlobalHeader from '../components/GlobalHeader';
+import GlobalFooter from '../components/GlobalFooter';
+import SiderMenu from '../components/SiderMenu';
+import NotFound from '../routes/Exception/404';
+
+const { Content } = Layout;
+
+const query = {
+  'screen-xs': {
+    maxWidth: 575,
+  },
+  'screen-sm': {
+    minWidth: 576,
+    maxWidth: 767,
+  },
+  'screen-md': {
+    minWidth: 768,
+    maxWidth: 991,
+  },
+  'screen-lg': {
+    minWidth: 992,
+    maxWidth: 1199,
+  },
+  'screen-xl': {
+    minWidth: 1200,
+  },
+};
+
+class BasicLayout extends React.PureComponent {
+  static childContextTypes = {
+    location: PropTypes.object,
+    breadcrumbNameMap: PropTypes.object,
+    routeData: PropTypes.array,
+  }
+  getChildContext() {
+    const { location, navData, getRouteData } = this.props;
+    const routeData = getRouteData('BasicLayout');
+    const firstMenuData = navData.reduce((arr, current) => arr.concat(current.children), []);
+    const menuData = this.getMenuData(firstMenuData, '');
+    const breadcrumbNameMap = {};
+
+    routeData.concat(menuData).forEach((item) => {
+      breadcrumbNameMap[item.path] = {
+        name: item.name,
+        component: item.component,
+      };
+    });
+    return { location, breadcrumbNameMap, routeData };
+  }
+  getPageTitle() {
+    const { location, getRouteData } = this.props;
+    const { pathname } = location;
+    let title = 'Ant Design Pro';
+    getRouteData('BasicLayout').forEach((item) => {
+      if (item.path === pathname) {
+        title = `${item.name} - Ant Design Pro`;
+      }
+    });
+    return title;
+  }
+  getMenuData = (data, parentPath) => {
+    let arr = [];
+    data.forEach((item) => {
+      if (item.name) {
+        arr.push({ path: `${parentPath}/${item.path}`, name: item.name });
+      }
+      if (item.children) {
+        arr = arr.concat(this.getMenuData(item.children, `${parentPath}/${item.path}`));
+      }
+    });
+    return arr;
+  }
+  render() {
+    const {
+      currentUser, collapsed, fetchingNotices, notices, getRouteData, navData, location, dispatch,
+    } = this.props;
+
+    const layout = (
+      <Layout>
+        <SiderMenu
+          collapsed={collapsed}
+          navData={navData}
+          location={location}
+          dispatch={dispatch}
+        />
+        <Layout>
+          <GlobalHeader
+            currentUser={currentUser}
+            fetchingNotices={fetchingNotices}
+            notices={notices}
+            collapsed={collapsed}
+            dispatch={dispatch}
+          />
+          <Content style={{ margin: '24px 24px 0', height: '100%' }}>
+            <div style={{ minHeight: 'calc(100vh - 260px)' }}>
+              <Switch>
+                {
+                  getRouteData('BasicLayout').map(item =>
+                    (
+                      <Route
+                        exact={item.exact}
+                        key={item.path}
+                        path={item.path}
+                        component={item.component}
+                      />
+                    )
+                  )
+                }
+                <Redirect exact from="/" to="/dashboard/analysis" />
+                <Route component={NotFound} />
+              </Switch>
+            </div>
+            <GlobalFooter
+              links={[{
+                title: 'Pro 首页',
+                href: 'http://pro.ant.design',
+                blankTarget: true,
+              }, {
+                title: 'GitHub',
+                href: 'https://github.com/ant-design/ant-design-pro',
+                blankTarget: true,
+              }, {
+                title: 'Ant Design',
+                href: 'http://ant.design',
+                blankTarget: true,
+              }]}
+              copyright={
+                <div>
+                  Copyright <Icon type="copyright" /> 2017 蚂蚁金服体验技术部出品
+                </div>
+              }
+            />
+          </Content>
+        </Layout>
+      </Layout>
+    );
+
+    return (
+      <DocumentTitle title={this.getPageTitle()}>
+        <ContainerQuery query={query}>
+          {params => <div className={classNames(params)}>{layout}</div>}
+        </ContainerQuery>
+      </DocumentTitle>
+    );
+  }
+}
+
+export default connect(state => ({
+  currentUser: state.user.currentUser,
+  collapsed: state.global.collapsed,
+  fetchingNotices: state.global.fetchingNotices,
+  notices: state.global.notices,
+}))(BasicLayout);
